26ec5f568a207c33ab00d3b7ec3325a0d2536715
rwiteshbera <rwiteshbera@gmail.com>
2022-03-19 00:42:01 +0530

    Updated UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,53 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
+import { InputGroup, FormControl } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import io from "socket.io-client";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-const serverURL = "http://localhost:8080/"
-const clientURL = "http://localhost:3001/"
+const serverURL = "http://localhost:8080/";
+const clientURL = "http://localhost:3001/";
 
 var socket = io(serverURL);
 
 function App() {
   const [inputURL, setInputURL] = useState("");
-  const [showShortedURL, setShowShortedURL] = useState("")
-  
+  const [showShortedURL, setShowShortedURL] = useState("");
+  const [copyButtonText, setCopyButtonText] = useState("Copy");
+
   const onChangeInput = (e) => {
     setInputURL(e.target.value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    socket.emit("sendInputURL", inputURL)
+    setCopyButtonText("Copy")
+    socket.emit("sendInputURL", inputURL);
     socket.on("sendURLtoClient", (uID) => {
-      setShowShortedURL(`${clientURL}${uID}`);
+      setShowShortedURL(`${uID}`);
     });
+  };
 
+  const Copy = () => {
+    setCopyButtonText("Copied");
+  };
+
+  const disableUIDchange = () => {
+
+  }
+
+  const visitURL = () => {
+    window.open(inputURL)
   }
 
   return (
     <div className="container-custom">
-      <h1>Hello, I am URL Shortener</h1>
+      <h1 id="heading">Hello, I am URL Shortener</h1>
 
       <form onSubmit={handleSubmit}>
         <input
+          placeholder="Shorten your URL"
           type="url"
           name="fullURL"
           id="fullURL"
@@ -39,11 +55,29 @@ function App() {
           value={inputURL}
           onChange={onChangeInput}
         />
-        <Button variant="success" type="submit">
+        <Button variant="success" type="submit" id="shorten-btn">
           Shorten
         </Button>
       </form>
-      <div id="showShortedURL">{showShortedURL}</div>
+        
+      <InputGroup className="mb-3" id="alias-form">
+        <Button variant="secondary" id="button-addon1">
+          {clientURL}
+        </Button>
+        <FormControl 
+          onChange={disableUIDchange}
+          value={showShortedURL}
+        />
+      </InputGroup>
+
+      <CopyToClipboard text={`${clientURL}${showShortedURL}`}>
+        <Button variant="primary" onClick={Copy}>
+          {copyButtonText}
+        </Button>
+      </CopyToClipboard>
+      <Button variant="primary" onClick={visitURL}>
+          Visit URL
+        </Button>
     </div>
   );
 }
